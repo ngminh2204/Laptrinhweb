0da65577efb577f4f0b35d531d7c883a3a8bffff
Nguyen Minh <119567787+ngminh2204@users.noreply.github.com>
2026-06-01 22:08:12 +0700

    Create fizzbuzz.js

---
+++ PBT_07/fizzbuzz.js
@@ -0,0 +1,55 @@
+// Version 1: Classic
+
+console.log("=== Classic FizzBuzz ===");
+
+for (let i = 1; i <= 100; i++) {
+
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    }
+    else if (i % 3 === 0) {
+        console.log("Fizz");
+    }
+    else if (i % 5 === 0) {
+        console.log("Buzz");
+    }
+    else {
+        console.log(i);
+    }
+}
+
+// Version 2: Custom
+
+function customFizzBuzz(n, rules) {
+
+    console.log("\n=== Custom FizzBuzz ===");
+
+    for (let i = 1; i <= n; i++) {
+
+        let result = "";
+
+        // Duyệt qua từng rule
+        for (let j = 0; j < rules.length; j++) {
+
+            if (i % rules[j].divisor === 0) {
+                result += rules[j].word;
+            }
+        }
+
+        // Nếu không khớp rule nào
+        if (result === "") {
+            console.log(i);
+        }
+        else {
+            console.log(i + " = " + result);
+        }
+    }
+}
+
+// Test
+
+customFizzBuzz(30, [
+    { divisor: 3, word: "Fizz" },
+    { divisor: 5, word: "Buzz" },
+    { divisor: 7, word: "Jazz" }
+]);
